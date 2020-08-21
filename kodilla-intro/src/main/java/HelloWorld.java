33cced45c24b181d02295cad4560ab5c88941e35
Pawel Duminski <pduminski@gmail.com>
2020-08-21 16:43:18 +0200

    kodilla-intro FirstClass and HelloWorld added

---
+++ kodilla-intro/src/main/java/HelloWorld.java
@@ -0,0 +1,8 @@
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println("Paweł");
+        System.out.println(29);
+        System.out.println('D');
+    }
+}
