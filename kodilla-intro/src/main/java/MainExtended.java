829261e9980cfcda96e1a983b4991aa28787c077
Pawel Duminski <pduminski@gmail.com>
2020-08-21 17:13:26 +0200

    MainExtended added

---
+++ kodilla-intro/src/main/java/MainExtended.java
@@ -0,0 +1,18 @@
+public class MainExtended {
+    public static void main(String[] args) {
+        String champion = "Barbarian";
+        int strength = 10;
+        double intelligence = 2.3;
+        boolean isTactical = false;
+        char attackerOrDefender = 'A';
+        byte yearsOfExperienceInBattle = 63;
+
+
+        System.out.println(champion + " stats: " );
+        System.out.println("str: " + strength);
+        System.out.println("int: " + intelligence);
+        System.out.println("is tactical: " + isTactical);
+        System.out.println("attacker or Defender: " + attackerOrDefender);
+        System.out.println("experience in battle: " + yearsOfExperienceInBattle + " years");
+    }
+}
