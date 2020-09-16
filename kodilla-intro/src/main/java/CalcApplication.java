2a4b9d17ff3049a25bab67ce5289274f7c4b8412
Pawel Duminski <pduminski@gmail.com>
2020-09-17 01:30:09 +0200

    static methods and variables

---
+++ kodilla-intro/src/main/java/CalcApplication.java
@@ -0,0 +1,16 @@
+public class CalcApplication {
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        double area = calculator.circleArea(10);
+        System.out.println("Area of the circle with radius 10 equals: " + area);
+
+
+        double p = Calculator.PI;
+        System.out.println("The value of PI constant is qeual to: " + p);
+
+        Calculator calculator2 = new Calculator();
+        System.out.println(calculator2.counter);
+
+    }
+
+}
