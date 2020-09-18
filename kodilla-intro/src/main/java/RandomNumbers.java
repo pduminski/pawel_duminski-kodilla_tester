3942b8e8528422b538f8c60c37b4b0f0f886a6c0
Pawel Duminski <pduminski@gmail.com>
2020-09-18 22:17:37 +0200

    refactor RandomNumbers

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -2,7 +2,7 @@ import java.util.Random;
 
 public class RandomNumbers {
     private int sumOfNumbers = 0;
-    private int lowestReturnedValue = 32; // out of bounds value
+    private int lowestReturnedValue = 31; // highest out of bounds
     private int highestReturnedValue = -1; // out of bounds value
 
     public void returnedNumberAfterReaching5000() {
@@ -11,33 +11,32 @@ public class RandomNumbers {
         int highestValue = 31; // 31 because the second value for generating int is exclusive not inclusive
 
         while (sumOfNumbers < 5000) {
-//                int number = rand.nextInt(highestValue - lowestValue) + lowestValue - highestValue;
-            int number = lowestValue +  (int)(Math.random()*(highestValue));
+            int number = rand.nextInt(31);
 
-            // lowest - to 100 jest po to, zeby przy pierwszym uruchomieniu przekazac juz warosc number / np tylko przy 1 iteracji
-            if (number <= lowestReturnedValue) {
-                lowestReturnedValue = number;
-            }
-
-            // highest - to 100 jest po to, zeby przy pierwszym uruchomieniu przekazac juz warosc number
-            if (number >= highestReturnedValue) {
-                highestReturnedValue = number;
-            }
+            getLowestReturnedValue(number);
+            getHighestReturnedValue(number);
 
             System.out.println("Generated Number: " + number + ", sum of the numbers: " + sumOfNumbers);
 
             sumOfNumbers += number;
-            continue;
-
         }
-        System.out.println("Sum of the numbers after reaching value 5000 is: " + sumOfNumbers);
     }
 
-    public int getLowestReturnedValue() {
+    public int getLowestReturnedValue(int number) {
+        if (number <= lowestReturnedValue) {
+            lowestReturnedValue = number;
+        }
         return lowestReturnedValue;
     }
 
-    public int getHighestReturnedValue() {
+    public int getHighestReturnedValue(int number) {
+        if (number >= highestReturnedValue) {
+            highestReturnedValue = number;
+        }
         return highestReturnedValue;
+    }
+
+    public void printHighestAndLowest() {
+        System.out.println("Highest: " + this.highestReturnedValue + " Lowest: " + this.lowestReturnedValue);
     }
 }
--- kodilla-intro/src/main/java/RandomNumbersTest.java
+++ kodilla-intro/src/main/java/RandomNumbersTest.java
@@ -2,7 +2,6 @@ public class RandomNumbersTest {
     public static void main(String[] args) {
         RandomNumbers rn = new RandomNumbers();
         rn.returnedNumberAfterReaching5000();
-        System.out.println("Najnizsza zwrocona wartosc: " + rn.getLowestReturnedValue());
-        System.out.println("Najwyzsza zwrocona wartosc: " + rn.getHighestReturnedValue());
+        rn.printHighestAndLowest();
     }
 }
