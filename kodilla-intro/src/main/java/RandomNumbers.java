797d904d2635610e6644afcbf658e880d4b25597
Pawel Duminski <pduminski@gmail.com>
2020-09-18 21:16:50 +0200

    powrot do starej wersji randomnumbers - zaraz bedzie refactor

--- kodilla-intro/src/main/java/RandomNumbers.java
+++ kodilla-intro/src/main/java/RandomNumbers.java
@@ -2,42 +2,42 @@ import java.util.Random;
 
 public class RandomNumbers {
     private int sumOfNumbers = 0;
-    private int lowestReturnedValue = -1; // out of bounds value
-    private int highestReturnedValue = 31; // out of bounds value
+    private int lowestReturnedValue = 32; // out of bounds value
+    private int highestReturnedValue = -1; // out of bounds value
 
     public void returnedNumberAfterReaching5000() {
         Random rand = new Random();
-        int lowestValue = 31;
-        int highestValue = 0; // 31 because the second value for generating int is exclusive not inclusive
+        int lowestValue = 0;
+        int highestValue = 31; // 31 because the second value for generating int is exclusive not inclusive
 
         while (sumOfNumbers < 5000) {
+//                int number = rand.nextInt(highestValue - lowestValue) + lowestValue - highestValue;
+            int number = lowestValue +  (int)(Math.random()*(highestValue));
 
-                int number = rand.nextInt(31);
-            System.out.println(number);
-                // lowest - to 100 jest po to, zeby przy pierwszym uruchomieniu przekazac juz warosc number / np tylko przy 1 iteracji
-                getLowestReturnedValue(number);
+            // lowest - to 100 jest po to, zeby przy pierwszym uruchomieniu przekazac juz warosc number / np tylko przy 1 iteracji
+            if (number <= lowestReturnedValue) {
+                lowestReturnedValue = number;
+            }
 
-                // highest - to 100 jest po to, zeby przy pierwszym uruchomieniu przekazac juz warosc number
-                getHighestReturnedValue(number);
+            // highest - to 100 jest po to, zeby przy pierwszym uruchomieniu przekazac juz warosc number
+            if (number >= highestReturnedValue) {
+                highestReturnedValue = number;
+            }
 
-                System.out.println("Generated Number: " + number + ", sum of the numbers: " + sumOfNumbers);
+            System.out.println("Generated Number: " + number + ", sum of the numbers: " + sumOfNumbers);
 
-                sumOfNumbers += number;
+            sumOfNumbers += number;
+            continue;
+
         }
         System.out.println("Sum of the numbers after reaching value 5000 is: " + sumOfNumbers);
     }
 
-    public int getLowestReturnedValue(int number) {
-        if (number <= lowestReturnedValue) {
-            lowestReturnedValue = number;
-        }
+    public int getLowestReturnedValue() {
         return lowestReturnedValue;
     }
 
-    public int getHighestReturnedValue(int number) {
-        if (number >= highestReturnedValue) {
-            highestReturnedValue = number;
-        }
+    public int getHighestReturnedValue() {
         return highestReturnedValue;
     }
 }
