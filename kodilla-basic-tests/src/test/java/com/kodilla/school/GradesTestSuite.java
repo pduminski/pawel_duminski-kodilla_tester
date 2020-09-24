5b330924d41acb71fcfc25a8cc6a259f9023ab28
Pawel Duminski <pduminski@gmail.com>
2020-09-24 21:06:19 +0200

    school

---
+++ kodilla-basic-tests/src/test/java/com/kodilla/school/GradesTestSuite.java
@@ -0,0 +1,46 @@
+package com.kodilla.school;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class GradesTestSuite {
+
+    @Test
+    public void shouldHaveZeroLength() {
+        Grades grades = new Grades();
+        int[] values = grades.getValues();
+        assertEquals(0, values.length);
+    }
+
+    @Test
+    public void shouldAddTwoElementsToTheArray() {
+        Grades grades = new Grades();
+        grades.add(4);
+        grades.add(2);
+
+        int[] values = grades.getValues();
+        assertEquals(2, values.length);
+        assertEquals(4, values[0]);
+        assertEquals(2, values[1]);
+    }
+
+    @Test
+    public void shouldCalculateAverage() {
+        Grades grades = new Grades();
+        grades.add(4);
+        grades.add(2);
+        grades.add(5);
+
+        double average = grades.getAverage();
+        assertEquals(3.66, average, 0.01);
+    }
+
+    @Test
+    public void shouldReturnZeroIfArrayIsEmpty() {
+        Grades grades = new Grades();
+
+        assertEquals(0, grades.getAverage(), 0.01);
+    }
+}
