4e8a6db9380286d5e81e01e8568497a4dbeab696
Pawel Duminski <pduminski@gmail.com>
2020-09-27 00:12:00 +0200

    interfaces continues

--- kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Triangle.java
+++ kodilla-collections/src/main/java/com/kodilla/collections/interfaces/Triangle.java
@@ -1,5 +1,21 @@
 package com.kodilla.collections.interfaces;
 
 public class Triangle implements Shape {
+    private double width;
+    private double height;
+    private double hypotenuse;
 
+    public Triangle(double width, double height, double hypotenuse) {
+        this.width = width;
+        this.height = height;
+        this.hypotenuse = hypotenuse;
+    }
+
+    public double getArea() {
+        return width * height / 2;
+    }
+
+    public double getPerimeter() {
+        return width + height + hypotenuse;
+    }
 }
