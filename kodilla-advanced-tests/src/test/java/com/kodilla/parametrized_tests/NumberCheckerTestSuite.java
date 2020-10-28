c45cd9ca629661fa5f00f359931bd372706c2bee
Pawel Duminski <pduminski@gmail.com>
2020-10-29 00:19:39 +0100

    junit works number checker

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/NumberCheckerTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/NumberCheckerTestSuite.java
@@ -1,6 +1,8 @@
 package com.kodilla.parametrized_tests;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -9,9 +11,17 @@ public class NumberCheckerTestSuite {
 
     private NumberChecker numberChecker = new NumberChecker();
 
-    @Test
-    public void shouldReturnTrueIfNumberEqualsThree() {
-        boolean result = numberChecker.isDivisibleByThree(3);
+    @ParameterizedTest
+    @ValueSource(ints = {0, 3, 6, -3, -12, 15})
+    public void shouldReturnTrueForNumberDivisibleByThree(int number) {
+        boolean result = numberChecker.isDivisibleByThree(number);
         assertTrue(result);
+    }
+
+    @ParameterizedTest
+    @ValueSource(ints = {-2, -1, 1, 2, 4, 5})
+    public void shouldReturnFalseForNumbersNotDivisibleByThree(int number) {
+        boolean result = numberChecker.isDivisibleByThree(number);
+        assertFalse(result);
     }
 }
