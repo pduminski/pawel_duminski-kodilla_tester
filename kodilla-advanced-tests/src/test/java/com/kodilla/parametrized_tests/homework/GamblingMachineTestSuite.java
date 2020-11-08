2a9a638976ca8e60c5a5316e0090d7103e6f1585
Pawel Duminski <pduminski@gmail.com>
2020-11-08 14:05:52 +0100

    gambling machine tests

--- kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
+++ kodilla-advanced-tests/src/test/java/com/kodilla/parametrized_tests/homework/GamblingMachineTestSuite.java
@@ -1,9 +1,11 @@
 package com.kodilla.parametrized_tests.homework;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.CsvFileSource;
 
 import java.util.HashSet;
+import java.util.Random;
 import java.util.Set;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -24,5 +26,33 @@ class GamblingMachineTestSuite {
         }
         System.out.println(userNumbers);
         assertEquals(0, machine.howManyWins(userNumbers));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/gamblingMachineNumbersWhichWillNotPassValidation.csv")
+    public void shouldThrowExceptionIfNumberOfElementsIsBelowOrOverTheRequiredSix(String numbers) throws InvalidNumbersException {
+        String[] stringWithNumbers = numbers.split(" ");
+        Set<Integer> userNumbers = new HashSet<>();
+        for (String number : stringWithNumbers) {
+            userNumbers.add(Integer.parseInt(number));
+        }
+        System.out.println(userNumbers);
+        assertThrows(InvalidNumbersException.class, () ->  machine.howManyWins(userNumbers));
+    }
+
+    @Test
+    public void shouldReturn0WinsOrMoreIfValidNumbersAreProvided() throws InvalidNumbersException{
+        Random random = new Random();
+        Set<Integer> userNumbers = new HashSet<>();
+
+        while(userNumbers.size() < 6) {
+            userNumbers.add(random.nextInt(49) + 1);
+        }
+        int numberOfWins = machine.howManyWins(userNumbers);
+
+        System.out.println(userNumbers);
+        System.out.println("Number of wins: " + numberOfWins);
+
+        assertTrue(numberOfWins >= 0);
     }
 }
