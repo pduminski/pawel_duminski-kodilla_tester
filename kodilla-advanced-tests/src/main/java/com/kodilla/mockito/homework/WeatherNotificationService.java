b4c3a717592702ac44fa5590407cd5934a579987
Pawel Duminski <pduminski@gmail.com>
2020-11-18 01:14:17 +0100

    stuff

--- kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherNotificationService.java
+++ kodilla-advanced-tests/src/main/java/com/kodilla/mockito/homework/WeatherNotificationService.java
@@ -1,44 +1,47 @@
 package com.kodilla.mockito.homework;
 
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
+import java.util.Set;
 
 public class WeatherNotificationService {
 
-    Map<Location, Subscriber> locationSubscriberHashMap = new HashMap<>();
-
-    public void addSubscriberToGivenLocation(Location location, Subscriber subscriber) {
-        locationSubscriberHashMap.put(location, subscriber);
-    }
-
-    public void removeSubscriberFromGivenLocation(Location location, Subscriber subscriber) {
-        locationSubscriberHashMap.remove(location, subscriber);
-    }
-
-    public void removeSubscriberFromAllLocations(Subscriber subscriber) {
-        for (Map.Entry<Location, Subscriber> locationSubscriberEntry : locationSubscriberHashMap.entrySet()) {
-            if (locationSubscriberEntry.getValue().equals(subscriber))
-                locationSubscriberHashMap.remove(locationSubscriberEntry.getKey());
-        }
-    }
-
-    public void sendGlobalNotification(WeatherNotification notification) {
-
-    }
-
-    public void sendNotificationForLocation(WeatherNotification notification, Location location) {
-        for (Map.Entry<Location, Subscriber> locationSubscriberEntry : locationSubscriberHashMap.entrySet())
-            if (locationSubscriberEntry.getKey().equals(location))
-                locationSubscriberEntry.getValue().receiveWeatherNotification(notification);
-    }
-
-    public void sendAllWeatherNotifications(WeatherNotification notification) {
-        for (Map.Entry<Location, Subscriber> locationSubscriberEntry : locationSubscriberHashMap.entrySet()) {
-            locationSubscriberEntry.getValue().receiveWeatherNotification(notification);
-        }
-    }
-
-    public Map<Location, Subscriber> getMap() {
-        return locationSubscriberHashMap;
-    }
+//    HashMap<Subscriber, Set<Location>> locationSubscriberHashMap = new HashMap<>();
+//
+//    public void addSubscriberToGivenLocation(Location location, Subscriber subscriber) {
+//        locationSubscriberHashMap.put(subscriber);
+//        locationSubscriberHashMap.computeIfAbsent(subscriber, k -> new HashSet<>()).add(location);
+//    }
+//
+//    public void removeSubscriberFromGivenLocation(Location location, Subscriber subscriber) {
+//        locationSubscriberHashMap.remove(location, subscriber);
+//    }
+//
+//    public void removeSubscriberFromAllLocations(Subscriber subscriber) {
+//        for (Map.Entry<Location, Subscriber> locationSubscriberEntry : locationSubscriberHashMap.entrySet()) {
+//            if (locationSubscriberEntry.getValue().equals(subscriber))
+//                locationSubscriberHashMap.remove(locationSubscriberEntry.getKey());
+//        }
+//    }
+//
+//    public void sendGlobalNotification(WeatherNotification notification) {
+//
+//    }
+//
+//    public void sendNotificationForLocation(WeatherNotification notification, Location location) {
+//        for (Map.Entry<Location, Subscriber> locationSubscriberEntry : locationSubscriberHashMap.entrySet())
+//            if (locationSubscriberEntry.getKey().equals(location))
+//                locationSubscriberEntry.getValue().receiveWeatherNotification(notification);
+//    }
+//
+//    public void sendAllWeatherNotifications(WeatherNotification notification) {
+//        for (Map.Entry<Location, Subscriber> locationSubscriberEntry : locationSubscriberHashMap.entrySet()) {
+//            locationSubscriberEntry.getValue().receiveWeatherNotification(notification);
+//        }
+//    }
+//
+//    public Map<Location, Subscriber> getMap() {
+//        return locationSubscriberHashMap;
+//    }
 }
